e7f17ce9b0fe98ab7ceec6452b3ef61e2cb99309
DiegoFischerDev <diegofischer.dev@gmail.com>
2023-02-02 19:21:09 -0300

    add App Load GIF

--- src/serviços/index.js
+++ src/serviços/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/" // http://localhost:3000/
+  baseURL: "https://8868-177-207-79-239.sa.ngrok.io/" // http://localhost:3000/
 })
